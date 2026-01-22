1fedfa160f5a3b64aa61eee953c7877727c5d77b
DeltaBaseCore <deltabasecore@gmail.com>
2026-01-22 18:33:28 +0100

    ejercicios

--- JS/Ejercicio_1A/main.js
+++ JS/Ejercicio_1A/main.js
@@ -2,6 +2,6 @@
 
 const lado1 = 16
 const lado2 = 23
-consta altura = 13
+const altura = 13
 const volumenCubo = lado1*lado2*altura
 console.log(volumenCubo)
---
+++ JS/Ejercicio_1B/main.js
@@ -0,0 +1,10 @@
+
+const cumpleanos = {
+    dia: 19,
+    mes: 'Noviembre'
+}
+
+console.log('Mi cumpleaños es el ' + cumpleanos.dia + ' de ' + cumpleanos.mes)
+
+console.log(`Mi cumpleaños es el ${cumpleanos.dia} de ${cumpleanos.mes}`)
+
