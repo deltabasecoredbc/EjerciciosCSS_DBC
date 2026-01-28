8de7e6c259696708d85c58d8d332cbeef336abdd
DeltaBaseCore <deltabasecore@gmail.com>
2026-01-28 18:59:24 +0100

    ejercicios

--- JS/Ejercicio_1G/main.js
+++ JS/Ejercicio_1G/main.js
@@ -1,90 +1,120 @@
 // Ejercicio 1G.1
 
-const numeros = [1,2,3,4,5]
+const numeros = [1, 2, 3, 4, 5];
 
-function sumaNumerosPares(numeros){
-    let suma = 0
+function sumaNumerosPares(numeros) {
+  let suma = 0;
 
-    for(const numero of numeros){
-        if(numero % 2 == 0){
-            suma += numero
-        }
+  for (const numero of numeros) {
+    if (numero % 2 == 0) {
+      suma += numero;
     }
+  }
 
-    return suma
+  return suma;
 }
 
-console.log('Ejercicio 1G.1 = ', sumaNumerosPares(numeros))
+console.log("Ejercicio 1G.1 = ", sumaNumerosPares(numeros));
 
 // Ejercicio 1G.2
 
+const cadena = "Hola Mundo";
 
-const cadena = 'Hola Mundo'
+function eliminarConsonantes(cadenaInput) {
+  let vocales = "";
 
-function eliminarConsonantes(cadenaInput){
-    let vocales = ''
+  const cadenaArray = cadenaInput.toLowerCase().split("");
 
-    const cadenaArray = cadenaInput.toLowerCase().split('')
+  for (const letra of cadenaArray) {
+    if (
+      letra == "a" ||
+      letra == "e" ||
+      letra == "i" ||
+      letra == "o" ||
+      letra == "u"
+    )
+      vocales += letra;
+  }
 
-    for(const letra of cadenaArray){
-        if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
-            vocales += letra
-    }
-
-    return vocales
+  return vocales;
 }
 
-console.log('Ejercicio 1G.2 = ', eliminarConsonantes(cadena))
-
+console.log("Ejercicio 1G.2 = ", eliminarConsonantes(cadena));
 
 // Ejercicio 1G.3
 
-
-function celsiusAFahrenheit(gradoCelsius){
-    return (gradoCelsius * (9/5) + 32)
+function celsiusAFahrenheit(gradoCelsius) {
+  return gradoCelsius * (9 / 5) + 32;
 }
 
-console.log('Ejercicio 1G.3 = ', celsiusAFahrenheit(20))
+console.log("Ejercicio 1G.3 = ", celsiusAFahrenheit(20));
 
 // Ejercicio 1G.4
 
-function checkNumber(numero){
-    if( numero % 2 == 0){
-        return 'Numero Par'
-    } else{
-        return 'Numero Impar'
-    }
+function checkNumber(numero) {
+  if (numero % 2 == 0) {
+    return "Numero Par";
+  } else {
+    return "Numero Impar";
+  }
 }
 
 // Ejercicio 1G.5
 
-const promptUsuario = prompt('Escribe la cadena a evaluar')
+const promptUsuario = prompt("Escribe la cadena a evaluar");
 
-function mayusOMinus(cadenaDeTexto){
-    if(promptUsuario == promptUsuario.toUpperCase()){
-        return alert('La cadena está en mayúsculas')
-    } else if (promptUsuario == promptUsuario.toLowerCase()){
-        return alert('La cadena está en minúsculas')
-    } else {
-        return alert('La cadena tiene mayúsculas y minúsculas')
-    }
+function mayusOMinus(cadenaDeTexto) {
+  if (promptUsuario == promptUsuario.toUpperCase()) {
+    return alert("La cadena está en mayúsculas");
+  } else if (promptUsuario == promptUsuario.toLowerCase()) {
+    return alert("La cadena está en minúsculas");
+  } else {
+    return alert("La cadena tiene mayúsculas y minúsculas");
+  }
 }
 
-mayusOMinus(promptUsuario)
-
+mayusOMinus(promptUsuario);
 
 // Ejercicio 1G.6
 
-const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles']
+const topics = [
+  "JavaScript",
+  "Variables",
+  "funciones",
+  "condicionales",
+  "bucles",
+];
 
-function invertirMayus(array){
-    const arrayMayus = array.map((topic)=>{
-        return topic.toUpperCase()
-    })
+function invertirMayus(array) {
+  const arrayMayus = array.map((topic) => {
+    return topic.toUpperCase();
+  });
 
-    arrayMayus.reverse()
-    return arrayMayus
+  arrayMayus.reverse();
+  return arrayMayus;
 }
 
-console.log('Ejercicio 1G.6 =', invertirMayus(topics))
+console.log("Ejercicio 1G.6 =", invertirMayus(topics));
 
+// Ejercicio 1G.7
+
+const number = [253, 8575, 1, 20, 562, 1233, 25, 27, 258, 254, 7485, 2683];
+
+function paresArray(arrayNumeros) {
+  return arrayNumeros.filter((numero) => numero % 2 == 0);
+}
+
+console.log("Ejercicio 1g.7 = ", paresArray(number));
+
+// Ejercicio 1G.8
+
+const names = ["marcos", "lucía", "susana", "rocío", "alfonso"];
+
+function nombresLargosMayusculas(arrayNames) {
+  return arrayNames
+    .filter((name) => name.length > 5)
+    .map((name) => {
+      return name.toUpperCase();
+    });
+}
+console.log("Ejercicio 1g.8 = ", nombresLargosMayusculas(names));
---
+++ JS/Ejercicio_1I/main.js
@@ -0,0 +1,19 @@
+const enlaces = document.getElementsByTagName("a");
+console.log("Número de enlaces es: ", enlaces.length);
+
+const penultimoEnlace = enlaces[enlaces.length - 2].href;
+console.log(penultimoEnlace);
+
+const tercerParrafo = document.getElementById("third-paragraph");
+const numeroEnlacesTercerParrafo =
+  tercerParrafo.getElementsByTagName("a").length;
+
+console.log(numeroEnlacesTercerParrafo);
+
+const parrafoResultados = document.createElement("p");
+const contenidoParrafoResultados = document.createTextNode(
+  `El número de enlaces en la página es: ${enlaces.length} , el link que apunta al penúltimo enlace es ${penultimoEnlace} y el número de enlaces del tercer párrafo es ${numeroEnlacesTercerParrafo}`,
+);
+
+parrafoResultados.appendChild(contenidoParrafoResultados);
+document.body.appendChild(parrafoResultados);
